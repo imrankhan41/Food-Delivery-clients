ea6740047b79e9268a51c044b3808f50a77fc9fe
imrankhanf41 <imranbutex41@gmail.com>
2021-10-31 18:38:54 +0600

    eigth

--- src/Pages/Home/PlaceOrder/PlaceOrder.js
+++ src/Pages/Home/PlaceOrder/PlaceOrder.js
@@ -28,12 +28,6 @@ const PlaceOrder = () => {
                 .then(res=>res.json())
                 .then(data =>setDetails(data))
             },[])
-
-            const handleNameChange=e=>{
-                const updatedName =e.target.value;
-                const updateUser ={name:updatedName, email:users?.displayName}
-                
-            }
     return (
         <div>
         {/* <h1>{serviceId}</h1>
@@ -58,9 +52,9 @@ const PlaceOrder = () => {
                  <h4>Email:{users?.email}</h4>
                  <img src={users?.photoURL} alt="" srcset="" />
                   <form className="add-user" onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register("name", { required: true, maxLength: 100 })} onChange={handleNameChange} placeholder="Name" value={users?.displayName || ''} />
-                    {/* <input {...register("img")}  placeholder="Personal Photo" value={users?.email || ''}/> */}
-                    <input {...register("email")}  placeholder="Your Email" value={users?.photoURL || ''}/>
+                    <input {...register("name", { required: true, maxLength: 100 })} placeholder="Name" value={users?.displayName} />
+                    <input {...register("img")}  placeholder="Personal Photo" value={users?.email}/>
+                    <input {...register("email")}  placeholder="Your Email" value={users?.photoURL}/>
                     <textarea {...register("address")} placeholder="Address"/>
                     <textarea {...register("city")} placeholder="City" />
                     <input type="number" {...register("price")} placeholder="Phone Number"/>
@@ -78,65 +72,3 @@ const PlaceOrder = () => {
 export default PlaceOrder;
 
 
-
-
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router';
-// import useAuth from '../../../hooks/useAuth';
-// import axios from 'axios';
-// import { useForm } from "react-hook-form";
-// import "./PlaceOrder.css"
-// const PlaceOrder = () => {
-//     const { register, handleSubmit,reset } = useForm();
-//     const onSubmit = data =>{console.log(data)};
-//     const {users,email}=useAuth();
-//     const {orderName}=useParams();
-//     const [details,setDetails]=useState([]);
-//     const [singleOrder,setSingleOrder]=useState({});
-//     useEffect(()=>{
-//         fetch("/services.json")
-//         .then(res=>res.json())
-//         .then(data =>setDetails(data))
-//     },[])
-//     useEffect(()=>{
-//         const foundDetails =details.find(orderDetails=>orderDetails.name==orderName)
-//         console.log(foundDetails);
-//         setSingleOrder(foundDetails)
-//     },[details])
-//     return (
-//         <>
-//         <div className="row ">
-//             <div className="col-lg-6">
-//             <div className="container details">
-//             <div className="mt-5 row">
-//                 <div className="col-lg-8 col-md-8 col-sm-12 col-12">
-//                 <h2>{singleOrder?.name}</h2>
-//                 <img className="img-fluid" src={singleOrder?.img} alt="" />
-//                 </div>
-//                 <div className="mt-5 text-start">
-//                     <h4>Price:${singleOrder?.price}</h4> <br />
-//                     {singleOrder?.description}
-//                 </div>
-//             </div>
-//         </div>
-//             </div>
-//             <div className="col-lg-6 booking text-secondary add1-user">
-//                 <h4>Ordered By:{users?.displayName}</h4>
-//                 <h4>Email:{users?.email}</h4>
-//                 <form className="add-user" onSubmit={handleSubmit(onSubmit)}>
-//                 <input {...register("name", { required: true, maxLength: 100 })} placeholder="Name" />
-//                 <input {...register("img")}  placeholder="Personal Photo"/>
-//                 <input {...register("email")}  placeholder="Your Email"/>
-//                 <textarea {...register("text")} placeholder="Address"/>
-//                 <textarea {...register("description")} placeholder="Description" />
-//                 <input type="number" {...register("price")} placeholder="Phone Number"/>
-//                 <input type="submit" />
-//                 </form>
-//             </div>
-//         </div>
-        
-//         </>
-//     );
-// };
-
-// export default PlaceOrder;
