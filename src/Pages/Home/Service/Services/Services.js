6b7dc251660d2d8b4a45567d2e241023bb349f1d
Imran Khan <86877944+imrankhan41@users.noreply.github.com>
2021-11-01 23:18:51 +0600

    Update Services.js

--- src/Pages/Home/Service/Services/Services.js
+++ src/Pages/Home/Service/Services/Services.js
@@ -4,24 +4,24 @@ import "./Services.css"
 const Services = () => {
     const [services,setServices]=useState([]);
     useEffect(()=>{
-        fetch("http://localhost:5000/services")
+        fetch("https://bloodcurdling-nightmare-55136.herokuapp.com/services")
         .then(res=>res.json())
         .then(data =>setServices(data))
     },[])
     return (
         <div id="services" className="m-auto mt-5 row ">
-            <h1 className="text-primary fw-bolder">Our Services</h1>
-          <div className="cards">
-          {
-                services.map(service=><Service
-                
-                key={service.name}
-                service={service}>
+        <h1 className="text-primary fw-bolder">Our Services</h1>
+      <div className="cards">
+      {
+            services.map(service=><Service
+            
+            key={service.name}
+            service={service}>
 
-                </Service>)
-            }
-          </div>
-        </div>
+            </Service>)
+        }
+      </div>
+    </div>
     );
 };
 
