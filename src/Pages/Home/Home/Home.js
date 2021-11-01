ae8ead98f59e13c59505d18f87d48f4eb38f6819
Imran Khan <86877944+imrankhan41@users.noreply.github.com>
2021-11-01 23:16:54 +0600

    Update Home.js

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,19 +1,16 @@
 import React from 'react';
-
 import Banner from '../Banner/Banner';
-import FrequentlyAskedQuestion from '../FrequentlyAskedQuestion/FrequentlyAskedQuestion';
 import ExtraService from '../ExtraService/ExtraService';
+import FrequentlyAskedQuestion from '../FrequentlyAskedQuestion/FrequentlyAskedQuestion';
 import Services from '../Service/Services/Services';
 
 const Home = () => {
     return (
         <div>
-       
             <Banner></Banner>
             <Services></Services>
             <FrequentlyAskedQuestion></FrequentlyAskedQuestion>
             <ExtraService></ExtraService>
-           
         </div>
     );
 };
